e3eef0b25d1e96eaaf11b41c6c957f639286e9b1
theano <theano@princeton.edu>
2021-10-08 11:10:45 -0400

    update setup stuff

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cd ~/.vim; git submodule update --init
 sudo apt install neovim
 echo vim="nvim" >> ~/.bash_aliases; source ~/.bash_aliases
 
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 cd ~/.config/nvim
 echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" > init.vim
 echo "let &packpath = &runtimepath" >> init.vim
